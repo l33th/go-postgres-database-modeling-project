884206dde6bee42322c32999d5cf0c90f5cca907
l33th <l33th@protonmail.ch>
2022-07-07 06:55:57 +0200

    docs: Basic code documentation

---
+++ db/db.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	// Third party packages
+	"github.com/go-pg/pg"
+
+	// Default packages
+	"log"
+	"os"
+)
+// Variable db is of type pg.DB
+var db *pg.DB
+
+// Connect will establish a database connection
+func Connect() {
+	// Credentials setup
+	options := &pg.Options {
+		User: "postgres",
+		Password: "lth-dev",
+		Addr: "localhost:5432",
+	}
+	// Connection setup
+	db = pg.Connect(options)
+	if db == nil {
+		log.Printf("Failed to connect to db\n")
+		os.Exit(100)
+	}
+	log.Println("Connection to db successful")
+
+	err := db.Close(nil)
+	if err != nil {
+		log.Printf("Error occured when trying to close the connection: %v\n", err)
+		os.Exit(100)
+	}
+	log.Println("Connection closed successfully")
+}
